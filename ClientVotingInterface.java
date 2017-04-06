ae09d3539f0a4809cbf5893ede81c7f2f85a1428
Anderson Singh <Anderson Singh>
2017-04-05 20:51:43 -0400

    added client remote files

---
+++ ClientVotingInterface.java
@@ -0,0 +1,17 @@
+package ClientVoting;
+
+import system.Candidate;
+
+import java.util.*;
+import java.rmi.*;
+
+
+public interface ClientVotingInterface extends Remote{
+
+	public ArrayList<Candidate> fetchCandidates() throws RemoteException;
+
+	public boolean castVote(String candidate) throws RemoteException;
+
+	public boolean canVote() throws RemoteException;
+
+}
